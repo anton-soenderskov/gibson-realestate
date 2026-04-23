f7c449220710643dcaa701987e2656ec51929197
anton-soenderskov <antonjimmyheino@gmail.com>
2026-04-23 14:34:24 +0200

    Initial build for the website

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import NavigationBar from './components/NavigationBar.vue'
+import Footer from './components/ui/Footer.vue'
+</script>
+
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <NavigationBar />
+    <main class="flex-1 overflow-auto">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-4xl font-bold mb-6">Gibson Real Estate</h1>
+    <p class="text-lg mb-4">Let's find your dream home together.</p>
+    <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
+      Search Listings By Map
+    </button>
+  </div>
+</template>
